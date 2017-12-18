01561e7395881b86f6d98f75456c0c26ed8ace39
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2017-12-18 17:31:11 -0500

    Add user login

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
   root 'posts#index'
 end
