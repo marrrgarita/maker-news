fb850d477a2d2c5169e59efab101ffc01c97c6d9
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2017-12-18 16:55:29 -0500

    Add post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218204858) do
+ActiveRecord::Schema.define(version: 20171218215221) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
