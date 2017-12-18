3a996e4b5854d3d60c2fb7ffa915f32b0f09a829
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2017-12-18 17:53:30 -0500

    Add ensure login to post creation/updating/deletion

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_login, except: [:index, :show]
 
   def index
     @posts = Post.all
