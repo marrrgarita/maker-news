b811f9daec3a98bd3757b8c6cc2fa0a9e9fc9523
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2017-12-21 13:24:32 -0500

    Serve static assets from public in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -29,8 +29,9 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
-  config.serve_static_assets = true
+  config.assets.enabled = true
+  config.assets.paths << Rails.root.join("public", "assets")
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
